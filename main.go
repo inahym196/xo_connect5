5c3d6bedce2eef1423030305ad80c7c90adc8d25
USER <user@USERnoMacBook-Pro.local>
2023-05-14 04:26:57 +0900

    introduce-gin

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-func main() {
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
 	board := make([][]int, 10)
 	for i := range board {
 		board[i] = make([]int, 10)
 	}
-	fmt.Println(board)
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, fmt.Sprint(board))
+	})
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	r.Run(":8080")
 }
